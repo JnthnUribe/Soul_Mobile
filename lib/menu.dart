d669ad7343d8f0d345bfbd724e539cdcd9d2cf00
jonadev19 <perezjony94@gmail.com>
2025-11-18 22:55:05 -0600

    nivel 2 completado creo

--- lib/menu.dart
+++ lib/menu.dart
@@ -2,6 +2,7 @@ import 'dart:async' as async;
 
 import 'package:bonfire/bonfire.dart';
 import 'package:darkness_dungeon/game.dart';
+import 'package:darkness_dungeon/level2.dart';
 import 'package:darkness_dungeon/shop/shop_screen.dart';
 import 'package:darkness_dungeon/util/custom_sprite_animation_widget.dart';
 import 'package:darkness_dungeon/util/enemy_sprite_sheet.dart';
@@ -56,134 +57,176 @@ class _MenuState extends State<Menu> {
               mainAxisSize: MainAxisSize.min,
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-              Text(
-                "Darkness Dungeon",
-                style: TextStyle(
-                    color: Colors.white, fontFamily: 'Normal', fontSize: 26.0),
-              ),
-              SizedBox(
-                height: 15.0,
-              ),
-              if (sprites.isNotEmpty)
-                SizedBox(
-                  height: 80,
-                  width: 80,
-                  child: CustomSpriteAnimationWidget(
-                    animation: sprites[currentPosition],
-                  ),
-                ),
-              SizedBox(
-                height: 20.0,
-              ),
-              SizedBox(
-                width: 150,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.deepPurple,
-                    foregroundColor: Colors.white,
-                    elevation: 3,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5.0),
-                    ),
-                    minimumSize: Size(100, 40),
-                  ),
-                  child: Text(
-                    getString('play_cap'),
-                    style: TextStyle(
+                Text(
+                  "Darkness Dungeon",
+                  style: TextStyle(
                       color: Colors.white,
                       fontFamily: 'Normal',
-                      fontSize: 17.0,
-                    ),
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => Game()),
-                    );
-                  },
+                      fontSize: 26.0),
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              SizedBox(
-                width: 150,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.amber[700],
-                    foregroundColor: Colors.black,
-                    elevation: 3,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5.0),
-                    ),
-                    minimumSize: Size(100, 40),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.shopping_cart, size: 18),
-                      SizedBox(width: 8),
-                      Text(
-                        'Tienda',
-                        style: TextStyle(
-                          fontFamily: 'Normal',
-                          fontSize: 17.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => ShopScreen()),
-                    );
-                  },
-                ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              Container(
-                width: 150,
-                child: DefectorRadio<bool>(
-                  value: false,
-                  label: 'Keyboard',
-                  group: Game.useJoystick,
-                  onChange: (value) {
-                    setState(() {
-                      Game.useJoystick = value;
-                    });
-                  },
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                width: 150,
-                child: DefectorRadio<bool>(
-                  value: true,
-                  group: Game.useJoystick,
-                  label: 'Joystick',
-                  onChange: (value) {
-                    setState(() {
-                      Game.useJoystick = value;
-                    });
-                  },
-                ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              if (!Game.useJoystick)
                 SizedBox(
-                  height: 70,
-                  width: 180,
-                  child: Sprite.load('keyboard_tip.png').asWidget(),
+                  height: 15.0,
                 ),
-            ],
-          ),
+                if (sprites.isNotEmpty)
+                  SizedBox(
+                    height: 80,
+                    width: 80,
+                    child: CustomSpriteAnimationWidget(
+                      animation: sprites[currentPosition],
+                    ),
+                  ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                SizedBox(
+                  width: 150,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.deepPurple,
+                      foregroundColor: Colors.white,
+                      elevation: 3,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                      minimumSize: Size(100, 40),
+                    ),
+                    child: Text(
+                      getString('play_cap'),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Normal',
+                        fontSize: 17.0,
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Game()),
+                      );
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                SizedBox(
+                  width: 150,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.amber[700],
+                      foregroundColor: Colors.black,
+                      elevation: 3,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                      minimumSize: Size(100, 40),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.shopping_cart, size: 18),
+                        SizedBox(width: 8),
+                        Text(
+                          'Tienda',
+                          style: TextStyle(
+                            fontFamily: 'Normal',
+                            fontSize: 17.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => ShopScreen()),
+                      );
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                // BOTÓN DE DESARROLLO - Ir directamente al Nivel 2
+                SizedBox(
+                  width: 150,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.orange[800],
+                      foregroundColor: Colors.white,
+                      elevation: 3,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                        side: BorderSide(color: Colors.orangeAccent, width: 2),
+                      ),
+                      minimumSize: Size(100, 40),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.bug_report, size: 16),
+                        SizedBox(width: 6),
+                        Text(
+                          'DEV: Nivel 2',
+                          style: TextStyle(
+                            fontFamily: 'Normal',
+                            fontSize: 15.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Level2()),
+                      );
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Container(
+                  width: 150,
+                  child: DefectorRadio<bool>(
+                    value: false,
+                    label: 'Keyboard',
+                    group: Game.useJoystick,
+                    onChange: (value) {
+                      setState(() {
+                        Game.useJoystick = value;
+                      });
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  width: 150,
+                  child: DefectorRadio<bool>(
+                    value: true,
+                    group: Game.useJoystick,
+                    label: 'Joystick',
+                    onChange: (value) {
+                      setState(() {
+                        Game.useJoystick = value;
+                      });
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                if (!Game.useJoystick)
+                  SizedBox(
+                    height: 70,
+                    width: 180,
+                    child: Sprite.load('keyboard_tip.png').asWidget(),
+                  ),
+              ],
+            ),
           ),
         ),
       ),
